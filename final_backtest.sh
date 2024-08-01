d7f109ee8eda0514a114a9ff97ac11af72b01b44
kayla <641141285@qq.com>
2024-08-01 19:06:20 +0100

    final_backtest added

---
+++ final_backtest.sh
@@ -0,0 +1,3 @@
+python Final_Backtest.py data/Cleaned_S_P_500_Data.csv predictions/sp500_predictions_varlingam_lag_1.csv 1 sp500 varlingam 4
+python Final_Backtest.py data/Cleaned_S_P_500_Data.csv predictions/sp500_predictions_varlingam_lag_2.csv 2 sp500 varlingam 4
+python Final_Backtest.py data/Cleaned_S_P_500_Data.csv predictions/sp500_predictions_varlingam_lag_3.csv 3 sp500 varlingam 4
