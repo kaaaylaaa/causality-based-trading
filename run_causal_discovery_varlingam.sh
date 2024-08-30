f5c073458c38a407f0cb731eca720536c629e27d
kayla <641141285@qq.com>
2024-08-30 11:41:26 +0800

    CD for latest sp500 data

--- run_causal_discovery_varlingam.sh
+++ run_causal_discovery_varlingam.sh
@@ -1,15 +1,18 @@
-python causal_discovery_varlingam.py data/pelosi-80.csv 1 nancy varlingam
-python causal_discovery_varlingam.py data/pelosi-80.csv 2 nancy varlingam
-python causal_discovery_varlingam.py data/pelosi-80.csv 3 nancy varlingam
-python causal_discovery_varlingam.py data/pelosi-80.csv 4 nancy varlingam
-python causal_discovery_varlingam.py data/pelosi-80.csv 5 nancy varlingam
-python causal_discovery_varlingam.py data/pelosi-80.csv 6 nancy varlingam
-python causal_discovery_varlingam.py data/sp500-80.csv 1 sp500 varlingam
-python causal_discovery_varlingam.py data/sp500-80.csv 2 sp500 varlingam
-python causal_discovery_varlingam.py data/sp500-80.csv 3 sp500 varlingam
-python causal_discovery_varlingam.py data/csi-300-80.csv 1 csi300 varlingam
-python causal_discovery_varlingam.py data/csi-300-80.csv 2 csi300 varlingam
-python causal_discovery_varlingam.py data/csi-300-80.csv 3 csi300 varlingam
-python causal_discovery_varlingam.py data/csi-300-80.csv 4 csi300 varlingam
-python causal_discovery_varlingam.py data/csi-300-80.csv 5 csi300 varlingam
-python causal_discovery_varlingam.py data/csi-300-80.csv 6 csi300 varlingam
+# python causal_discovery_varlingam.py data/pelosi-80.csv 1 nancy varlingam
+# python causal_discovery_varlingam.py data/pelosi-80.csv 2 nancy varlingam
+# python causal_discovery_varlingam.py data/pelosi-80.csv 3 nancy varlingam
+# python causal_discovery_varlingam.py data/pelosi-80.csv 4 nancy varlingam
+# python causal_discovery_varlingam.py data/pelosi-80.csv 5 nancy varlingam
+# python causal_discovery_varlingam.py data/pelosi-80.csv 6 nancy varlingam
+# python causal_discovery_varlingam.py data/sp500-80.csv 1 sp500 varlingam
+# python causal_discovery_varlingam.py data/sp500-80.csv 2 sp500 varlingam
+# python causal_discovery_varlingam.py data/sp500-80.csv 3 sp500 varlingam
+# python causal_discovery_varlingam.py data/csi-300-80.csv 1 csi300 varlingam
+# python causal_discovery_varlingam.py data/csi-300-80.csv 2 csi300 varlingam
+# python causal_discovery_varlingam.py data/csi-300-80.csv 3 csi300 varlingam
+# python causal_discovery_varlingam.py data/csi-300-80.csv 4 csi300 varlingam
+# python causal_discovery_varlingam.py data/csi-300-80.csv 5 csi300 varlingam
+# python causal_discovery_varlingam.py data/csi-300-80.csv 6 csi300 varlingam
+python causal_discovery_varlingam.py data/Cleaned_SP500_Data_latest.csv 1 latest_sp500 varlingam
+python causal_discovery_varlingam.py data/Cleaned_SP500_Data_latest.csv 2 latest_sp500 varlingam
+python causal_discovery_varlingam.py data/Cleaned_SP500_Data_latest.csv 3 latest_sp500 varlingam
